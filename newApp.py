ac2a15a0c4b38e8d70afb0485c744b1a623aa1ad
Jacob Temple <jacobbtemple@gmail.com>
2017-11-08 17:59:43 -0600

    more changes to newApp.py

--- newApp.py
+++ newApp.py
@@ -1,5 +1,10 @@
-from flask import Flask
-from flask import render_template # Import render_template function
+from flask import Flask, render_template, request, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Book, engine
+from create_db import create_book, session
+import subprocess
+import json
 
 app = Flask(__name__)
 
@@ -13,28 +18,57 @@ def about():
 
 @app.route('/books')
 def books():
-	return render_template('books.html')
+	books = session.query(Book).all()
+	return render_template('newBooks.html', books = books)
 
 @app.route('/publishers')
 def publishers():
-	return render_template('publishers.html')
+	publishers = session.query(Book).distinct(Book.publisher_name)
+	return render_template('publishers.html', publishers = publishers)
 
 @app.route('/authors')
 def authors():
-	return render_template('authors.html')
+	authors = session.query(Book).distinct(Book.author_name)
+	return render_template('authors.html', authors = authors)
 
 @app.route('/authors/<name>')
-def author_template(name):
-	info = session.query(authors).filter_by(name=name)
-	birthday = info.birthday.first() #Depends on table call name
-	education = info.education.first() #Depends on table call name
-	nationality = info.nationality.first() #Depends on table call name
-	description = info.description.first() #Depends on table call name
-	alma_mater = info.alma_mater.first() #Depends on table call name
-	wiki_url = info.wiki_url.first() #Depends on table call name
-	image_url = info.image_url.first() #Depends on table call name
-	books = info.books #Depends on table call name
-	return render_template('author_template.html', name=name, born=born, education=education, nationality=nationality, description=description, alma_mater=alma_mater, author_wikipedia_url=author_wikipedia_url, author_image_url=author_image_url, books=books)
+def author_page(name):
+	# if do not add ".first()" to the end of this, the default data will look like a list of dictionaries
+	info = session.query(Book).filter_by(author_name=name)
+	birthday = info.first().born
+	education = info.first().education
+	nationality = info.first().nationality
+	description = info.first().author_description
+	alma_mater = info.first().alma_mater
+	wikipedia_url = info.first().author_wikipedia_url
+	image_url = info.first().author_image_url
+
+	return render_template('author_template.html', info=info, name=name, birthday=birthday, education=education, nationality=nationality,\
+	description=description, alma_mater=alma_mater, wikipedia_url=wikipedia_url, image_url=image_url)
+	# the list of books is passed through info. It needs to be done this way because we must use a loop to create
+	# the table of all books by the author
+
+@app.route('/books/<title>')
+def book_page(title):
+	info = session.query(Book).filter_by(title=title)
+	google_id = info.first().google_id
+	isbn = info.first().isbn
+	publication_date = info.first().publication_date
+	description = info.first().book_description
+	image_url = info.first().book_image_url
+
+	return render_template('book_template.html', info=info, title=title, google_id=google_id, isbn=isbn, publication_date = publication_date,\
+	description=description, image_url=image_url)
+
+#@app.route('/publishers/<publisher>')
+#def author_page(publisher):
+	#todo, test authors first
+
+@app.route('/unit_tests')
+def unit_tests():
+  output = subprocess.getoutput("python test.py")
+  return json.dumps({'output': str(output)})
+
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
