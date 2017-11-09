0b1d0566aa2d10ea915be8bf95486ae9d9466ed5
shayaanj <shayaan.jagtap@gmail.com>
2017-11-08 18:31:35 -0600

    updated models and create_db conflicts

--- create_db.py
+++ create_db.py
@@ -6,26 +6,7 @@ import json, logging
 # and the empty database we created into our environment
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
-from models import Base, Book, engine, Publisher, Author
-
-# bind the engine to the base class. This makes the connection
-# between our class definitions and the corresponding tables
-# within our database
-Base.metadata.bind = engine
-
-# create session maker object to establish a link
-# of communication between our code execution and
-# the engine we just created
-DBSession = sessionmaker(bind=engine)
-
-# in order to create, read, update or delete information
-# on our database, SQLAlchmey executes database operations
-# via an interface called a session.
-# A session allows us to write down all the commands
-# we want to execute but not send them to the DB
-# until we call "commit"
-# create an instance of DBSession
-session = DBSession()
+from models import Base, Book, engine
 
 # bind the engine to the base class. This makes the connection
 # between our class definitions and the corresponding tables
@@ -53,83 +34,83 @@ def load_json(filename):
 
     return jsn
 
-<<<<<<< HEAD
-def create_books():
-
-    book = load_json('books.json')
-
-    for oneBook in book['Books']:
-        title       = oneBook['title']
-        google_id   = oneBook['google_id']
-        isbn        = oneBook['isbn']
-        pub_date    = oneBook['publication_date']
-        img_url     = oneBook['image_url']
-        description = oneBook['description']
-        publisher   = oneBook['publisher'][0]['name'] # needs to be changed
-        author      = oneBook['author'][0]['name']
-
-        for publisher in oneBook['publishers']:
-            wikipedia_url = publisher["wikipedia_url"]
-            name          = publisher["name"]
-            description   = publisher["description"]
-            owner         = publisher["owner"]
-            image_url     = publisher["image_url"]
-            website       = publisher["website"]
-
-            # modify the argument names as needed for the database class
-            newPublisher = Publisher(wiki_Url    = wikipedia_url
-                                     name        = name
-                                     description = description
-                                     owner       = owner
-                                     image_url   = image_url
-                                     website     = website
-                                    )
-
-            session.add(newPublisher)
-            session.commit() # unsure if needed or one at the very end
-
-        for author in oneBook['authors']:
-            born          = author["born"]
-            name          = author["name"]
-            education     = author["education"]
-            nationality   = author["nationality"]
-            description   = author["description"]
-            alma_mater    = author["alma_mater"]
-            wikipedia_url = author["wikipedia_url"]
-            image_url     = author["image_url":]
-
-            # modify the argument names as needed for the database class
-            newAuthor = Author(born          = born
-                               name          = name
-                               education     = education
-                               nationality   = nationality
-                               description   = description
-                               alma_mater    = alma_mater
-                               wikipedia_url = wikipedia_url
-                               image_url     = image_url
-                               )
-
-            session.add(newAuthor)
-            session.commit() # unsure if needed here or one at the very end
-
-        # modify the argument names as needed for the database class
-        newBook = Book(title       = title,
-                       id          = google_id,
-                       isbn        = isbn,
-                       pub_date    = pub_date,
-                       img_url     = img_url,
-                       description = description
-                       publisher   = publisher
-                       )
-
-        session.add(newBook)
-
-        session.commit() # unsure if needed
-
-    session.commit() # unsure if needed or one after each book/author/publisher
-
-create_books()
-=======
+# <<<<<<< HEAD
+# def create_books():
+#
+#     book = load_json('books.json')
+#
+#     for oneBook in book['Books']:
+#         title       = oneBook['title']
+#         google_id   = oneBook['google_id']
+#         isbn        = oneBook['isbn']
+#         pub_date    = oneBook['publication_date']
+#         img_url     = oneBook['image_url']
+#         description = oneBook['description']
+#         publisher   = oneBook['publisher'][0]['name'] # needs to be changed
+#         author      = oneBook['author'][0]['name']
+#
+#         for publisher in oneBook['publishers']:
+#             wikipedia_url = publisher["wikipedia_url"]
+#             name          = publisher["name"]
+#             description   = publisher["description"]
+#             owner         = publisher["owner"]
+#             image_url     = publisher["image_url"]
+#             website       = publisher["website"]
+#
+#             # modify the argument names as needed for the database class
+#             newPublisher = Publisher(wiki_Url    = wikipedia_url
+#                                      name        = name
+#                                      description = description
+#                                      owner       = owner
+#                                      image_url   = image_url
+#                                      website     = website
+#                                     )
+#
+#             session.add(newPublisher)
+#             session.commit() # unsure if needed or one at the very end
+#
+#         for author in oneBook['authors']:
+#             born          = author["born"]
+#             name          = author["name"]
+#             education     = author["education"]
+#             nationality   = author["nationality"]
+#             description   = author["description"]
+#             alma_mater    = author["alma_mater"]
+#             wikipedia_url = author["wikipedia_url"]
+#             image_url     = author["image_url":]
+#
+#             # modify the argument names as needed for the database class
+#             newAuthor = Author(born          = born
+#                                name          = name
+#                                education     = education
+#                                nationality   = nationality
+#                                description   = description
+#                                alma_mater    = alma_mater
+#                                wikipedia_url = wikipedia_url
+#                                image_url     = image_url
+#                                )
+#
+#             session.add(newAuthor)
+#             session.commit() # unsure if needed here or one at the very end
+#
+#         # modify the argument names as needed for the database class
+#         newBook = Book(title       = title,
+#                        id          = google_id,
+#                        isbn        = isbn,
+#                        pub_date    = pub_date,
+#                        img_url     = img_url,
+#                        description = description
+#                        publisher   = publisher
+#                        )
+#
+#         session.add(newBook)
+#
+#         session.commit() # unsure if needed
+#
+#     session.commit() # unsure if needed or one after each book/author/publisher
+#
+# create_books()
+# =======
 def create_book():
     books = load_json('books.json')
     for oneBook in books:
@@ -223,4 +204,3 @@ def create_book():
         session.commit()
 
 create_book()
->>>>>>> master
