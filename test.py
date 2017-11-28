5f656f8d5e74885a13b0e21c573549a8d2a2f70c
JacobVanderlinden <jacobvanderlinden.0@gmail.com>
2017-11-28 10:06:54 -0600

    Added everyone's unit tests and link to presentation

--- test.py
+++ test.py
@@ -44,5 +44,55 @@ class DBTestCases(unittest.TestCase):
 		session.query(Test).filter_by(title='mybook3').delete()
 		session.commit()
 
+	def test_source_insert_4(self):
+		s4 = Test(title='mybook4', publisher_name = 'publisher4')
+		session.add(s4)
+
+		r4 = session.query(Test).filter_by(title='mybook4').one()
+		self.assertEqual(str(r4.publisher_name), 'publisher4')
+
+		session.query(Test).filter_by(title='mybook4').delete()
+		session.commit()
+
+	def test_source_insert_5(self):
+		s5 = Test(title='mybook5', publisher_name = 'publisher5')
+		session.add(s5)
+
+		r5 = session.query(Test).filter_by(title='mybook5').one()
+		self.assertEqual(str(r5.publisher_name), 'publisher5')
+
+		session.query(Test).filter_by(title='mybook5').delete()
+		session.commit()
+
+	def test_source_insert_6(self):
+		s6 = Test(title='mybook6', publisher_name = 'publisher6')
+		session.add(s6)
+
+		r6 = session.query(Test).filter_by(title='mybook6').one()
+		self.assertEqual(str(r6.publisher_name), 'publisher6')
+
+		session.query(Test).filter_by(title='mybook6').delete()
+		session.commit()
+
+	def test_source_insert_7(self):
+		s7 = Test(title='mybook7', publisher_name = 'publisher7')
+		session.add(s7)
+
+		r7 = session.query(Test).filter_by(title='mybook7').one()
+		self.assertEqual(str(r7.publisher_name), 'publisher7')
+
+		session.query(Test).filter_by(title='mybook7').delete()
+		session.commit()
+
+	def test_source_insert_8(self):
+		s8 = Test(title='mybook8', publisher_name = 'publisher8')
+		session.add(s8)
+
+		r8 = session.query(Test).filter_by(title='mybook8').one()
+		self.assertEqual(str(r8.publisher_name), 'publisher8')
+
+		session.query(Test).filter_by(title='mybook8').delete()
+		session.commit()
+
 if __name__ == '__main__':
 	unittest.main()
